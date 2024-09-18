bfaac8418bfa4ef4c65768fd131470c56b78a2e9
David Carlos <davidcarlosfs@hotmail.com>
2024-09-18 17:32:40 -0300

    update sistema_bancario add new methods

--- sitema_bancario/func_sistema_bancario.py
+++ sitema_bancario/func_sistema_bancario.py
@@ -1,31 +1,73 @@
+from datetime import datetime
+import pytz
+
+class Usuario:
+    def __init__(self, nome: str, data_nascimento: str, cpf: str, endereco: str):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = endereco
+
 class ContaBancaria():
     def __init__(self):
         self.depositos = []
         self.saques = []
         self.saldo = 0
         self.saques_dias = 3
+        self.transacoes = 10
+        self.usuarios = []
+        self.usuario_cpf = {}  # Alterado para um dicionário
+        self.contas = []
+        self.numero_conta = 0
 
-    def depositar(self, deposito):
+        self.AGENCIA = "0001"
+
+    def criar_usuario(self, nome: str, data_nascimento: str, cpf: str, endereco: str):
+        if cpf in self.usuario_cpf:
+            print("CPF já cadastrado, por favor, retorne ao menu e informe um CPF diferente")
+            return
         
-        if deposito >= 0:
-            self.depositos.append(deposito)
+        novo_usuario = Usuario(nome, data_nascimento, cpf, endereco)
+        self.usuarios.append(novo_usuario)
+        self.usuario_cpf[cpf] = []  # Inicialmente, o CPF não tem contas
+        print(f"Usuário {nome} foi cadastrado com sucesso!")
+    
+    def criar_conta(self, cpf):
+        if cpf in self.usuario_cpf:
+            self.numero_conta += 1
+            conta = {"agencia": self.AGENCIA, "numero_conta": self.numero_conta, "usuario_cpf": cpf}
+            self.contas.append(conta)
+            self.usuario_cpf[cpf].append(self.numero_conta)  # Adiciona o número da conta à lista de contas do CPF
+            print(f"Conta criada com sucesso!")
+        else:
+            print("Usuário não encontrado, por favor, retorne ao menu e informe um CPF já cadastrado.")
+            return
+
+    def depositar(self, deposito: float):
+        if self.transacoes < 1:
+            print("Você chegou ao limite de transações diárias, por favor retorne outro dia!")
+        elif deposito <= 0:
+            print("Você digitou um valor incorreto, por favor, retorne ao menu inicial e refaça a operação!")    
+        elif deposito > 0:
+            self.transacoes -= 1
+            self.depositos.append((deposito, datetime.now(pytz.timezone("America/Sao_Paulo"))))
             self.saldo += deposito
             print(f"Você depositou R${deposito}, seu saldo atual é R${self.saldo}!")
-        else:
-            print("Você digitou um valor incorreto, por favor, retorne ao menu inicial e refaça a operação!")
 
-    def sacar(self, saque):
-
-        if self.saques_dias < 1:
+    def sacar(self, *, saque: float):
+        if self.transacoes < 1:
+            print("Você chegou ao limite de transações diárias, por favor retorne outro dia!")
+        elif self.saques_dias < 1:
             print("Você chegou ao limite de saque do dia, por favor faça outra operação ou retorne outro dia!")
         elif saque > 500:
             print("Você ultrapassou o limite do valor de saque permitido, por favor, refaça a operação e tente outro valor.")
-        elif saque > self.saldo or self.saldo < 0:
+        elif saque > self.saldo or self.saldo <= 0:
             print("Saldo insuficiente, por favor refaça a operação e tente outro valor.")
         else:
-            self.saques.append(saque)
-            self.saldo -= saque
+            self.transacoes -= 1
             self.saques_dias -= 1
+            self.saques.append((saque, datetime.now(pytz.timezone("America/Sao_Paulo"))))
+            self.saldo -= saque
             print(f"Você sacou R${saque}, seu saldo atual é R${self.saldo}!")
 
     def extrato(self):
@@ -36,13 +78,47 @@ class ContaBancaria():
         print("Extrato:")
         if self.depositos:
             print("Depósitos:")
-            for deposito in self.depositos:
-                print(f"R${deposito:.2f}")
+            for deposito, data_hora in self.depositos:
+                print(f"{data_hora.strftime('%d/%m/%Y %H:%M:%S')} - Depósito: R${deposito:.2f}")
         
         if self.saques:
             print("Saques:")
-            for saque in self.saques:
-                print(f"R${saque:.2f}")
+            for saque, data_hora in self.saques:
+                print(f"{data_hora.strftime('%d/%m/%Y %H:%M:%S')} - Saque: R${saque:.2f}")
         
         print("Saldo:")
-        print(self.saldo)
+        print(f"R${self.saldo:.2f}")
+
+    def encontrar_contas_por_cpf(self, cpf: str):
+        if cpf not in self.usuario_cpf:
+            print("CPF não encontrado!")
+        elif not self.usuario_cpf[cpf]:
+            print("Não há contas vinculadas ao CPF informado!")
+        else:
+            contas_vinculadas = [conta for conta in self.contas if conta["usuario_cpf"] == cpf]
+            if contas_vinculadas:
+                print(f"Contas vinculadas ao CPF {cpf}:")
+                for conta in contas_vinculadas:
+                    print(f"Agência: {conta['agencia']}, Número da Conta: {conta['numero_conta']}")
+            else:
+                print("Não há contas vinculadas ao CPF informado!")
+
+    def deletar_conta(self, cpf: str, numero_conta: int):
+        if cpf not in self.usuario_cpf:
+            print("CPF não encontrado!")
+            return
+        
+        # Verifica se o CPF tem contas associadas
+        if numero_conta not in self.usuario_cpf[cpf]:
+            print("Conta não encontrada para o CPF informado.")
+            return
+        
+        # Remove a conta da lista de contas
+        conta_a_deletar = next((conta for conta in self.contas if conta["numero_conta"] == numero_conta and conta["usuario_cpf"] == cpf), None)
+        
+        if conta_a_deletar:
+            self.contas.remove(conta_a_deletar)
+            self.usuario_cpf[cpf].remove(numero_conta)  # Remove o número da conta da lista de contas do CPF
+            print(f"Conta número {numero_conta} vinculada ao CPF {cpf} foi deletada com sucesso!")
+        else:
+            print("Conta não encontrada para o CPF informado.")
--- sitema_bancario/menu_sistema_bancario.py
+++ sitema_bancario/menu_sistema_bancario.py
@@ -1,27 +1,52 @@
 from func_sistema_bancario import *
 conta = ContaBancaria()
-menu = 5
+
+menu = 9
 
 while menu != 0:
     menu = int(input(
                  """
                  ============MENU=============
-                 [1] Digite 1 para depositar\n
-                 [2] Digite 2 para sacar\n
-                 [3] Digite 3 para ver o extrato\n
+                 [1] Digite 1 para adcionar um novo usuário\n
+                 [2] Digite 2 para criar uma conta\n
+                 [3] Digite 3 para acessar a lista de contas do usuário\n
+                 [4] Digite 4 para deletar uma conta\n
+                 [5] Digite 5 para depositar\n
+                 [6] Digite 6 para sacar\n
+                 [7] Digite 7 para ver o extrato\n
                  [0] Digite 0 para encerrar a operação
                  """
                  ))
     
     if menu == 1:
-        deposito = int(input("Digite o valor a ser depositado: R$"))
-        conta.depositar(deposito)
-    
+        nome_usuário = input("Digite o nome do usuário: ")
+        data_nascimento_usuario = input("Digite a data de nascimento do usuário (dd-mm-aaaa): ")
+        cpf_usuario = input("Digite o CPF do usuario (somente números): ")
+        endereco_usuario = input("Digite o endereço do usuário (logradouro, nº, bairo - cidade/sigla estado): ")
+        conta.criar_usuario(nome_usuário, data_nascimento_usuario,cpf_usuario,endereco_usuario)
+
     elif menu == 2:
-        saque = int(input("Digite o valor a ser sacado: R$"))
-        conta.sacar(saque)
-    
+        cpf = input("Digite o número do CPF de usuário já cadastrado: ")
+        conta.criar_conta(cpf)
+
     elif menu == 3:
+        usuario = input("Digite o CPF do usuário que deseja encontrar alguma conta: ")
+        conta.encontrar_contas_por_cpf(cpf=usuario)
+
+    elif menu == 4:
+        cpf_deletar = input("Digite o CPF da conta que deseja deletar: ")
+        numero_conta = input("Digite o número da conta que deseja deletar: ")
+        conta.deletar_conta(cpf=cpf_deletar, numero_conta=numero_conta)
+
+    elif menu == 5:
+        deposito = float(input("Digite o valor a ser depositado: R$"))
+        conta.depositar(deposito)
+
+    elif menu == 6:
+        saque = float(input("Digite o valor a ser sacado: R$"))
+        conta.sacar(saque=saque)
+    
+    elif menu == 7:
         conta.extrato()
     
     elif menu == 0:
